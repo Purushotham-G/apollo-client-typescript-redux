4836423205d89cfd4c0021a6b9f143d48a3db340
purushotham <purushu1995@gmail.com>
2023-12-20 15:23:17 +0530

    latest merged the the code

---
+++ src/component/TwoFunction.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+
+export const TwoFunction = () => {
+  return (
+    <div>TwoFunction</div>
+  )
+}
+
+function a (){
+    return (
+        <>
+        <p>hello a function</p>
+        </>
+    )
+}
+a()
+function b (){
+    return (
+        <>
+        <p>hello b function</p>
+        </>
+    )
+}
+b()
